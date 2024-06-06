ea42c0218f1f78a49d792fcf1ea4699ba495676e
Ikerpaipai <ikerpaillet@gmail.com>
2024-06-06 12:37:23 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,106 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import bcrypt from 'bcrypt';
+import cors from 'cors';
+import knex from 'knex';
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: 'postgres',
+        password: 'test',
+        database: 'smartbrain'
+    }
+});
+
+db.select('*').from('users').then(data => {
+    console.log(data);
+})
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req,res)=>{
+    res.send(database.users)
+})
+
+app.post("/signin", (req, res) => {
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if(isValid){
+                db.select('*').from('users')
+                .where('email', '=', req.body.email)
+                .then(user => {
+                    res.json(user[0])
+                })
+                .catch(err => res.status(400).json("unable get user"))
+            }
+        })
+        .catch(err => res.status(400).json("wrong credencials"))
+})
+
+app.post("/register", (req,res) => {
+    const {name, email, password} = req.body;
+    const hash = bcrypt.hashSync(password, 10);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(function(loginEmail){
+            console.log("Inserted into login table: ", loginEmail);
+            trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date()
+            })
+            .then(user => {
+                console.log('Resolve user: ',user);
+                res.json(user[0])
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => {
+        res.status(400).json('unable to register');
+    });
+})
+
+app.get("/profile/:id", (req,res)=>{
+    const {id} = req.params;
+    db.select('*').from('users').where({id})
+    .then(user =>{
+        if(user.length){
+            res.json(user[0]);
+        }else{
+            res.status(400).json('Not found')
+        }
+    })
+})
+
+app.put("/image", (req,res)=>{
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+})
+
+app.listen(port, ()=>{
+    console.log("Server is running on port: ", port);
+})
