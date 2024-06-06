d30562dda1731e0f2677e2e78ee30f02f75badf2
Ikerpaipai <ikerpaillet@gmail.com>
2024-06-06 16:11:33 +0200

    maj db render

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ dotenv.config();
 
 const db = knex({
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: {
+        host: process.env.DB_HOST,
+        port: 5432,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }
 });
 
 db.select('*').from('users').then(data => {
