100467a50eca083044a1ad88ea438fb532c06ea6
Ikerpaipai <ikerpaillet@gmail.com>
2024-06-07 19:58:38 +0200

    maj ssl

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ const db = knex({
         port: 5432,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
